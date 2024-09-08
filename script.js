b2d2ea82e73dfeeb14543ba8ae93dedd821324bc
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-09-09 02:49:08 +0530

    up

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 function shrinkOverlay() {
     const left = document.getElementById('left');
     const right = document.getElementById('right');
-    left.classList.add('shrinklr');  // Add the class to trigger the shrinking animation
-    right.classList.add('shrinklr');  // Add the class to trigger the shrinking animation
+    left.classList.add('shrinklr');  
+    right.classList.add('shrinklr');
     const top = document.getElementById('top');
     const bottom = document.getElementById('bottom');
-    top.classList.add('shrinktb');  // Add the class to trigger the shrinking animation
-    bottom.classList.add('shrinktb');  // Add the class to trigger the shrinking animation
+    top.classList.add('shrinktb'); 
+    bottom.classList.add('shrinktb'); 
     const logo = document.getElementById('logo');
     const login = document.getElementById('login');
     logo.classList.remove('icon');
-    // Optionally hide the overlay after the animation completes
+    const celeb = document.getElementById('celeb');
+     celeb.style.display = "none"
+  
     setTimeout(() => {
         login.classList.add('mv');
-    }, 1000);  // Matches animation duration
+    }, 1000);  
+}
+
+
+function login() {
+    const btn = document.getElementById('btn');
+    btn.innerHTML="";
+    btn.innerHTML=`  <div class="spinner"></div>`;
+
+ 
+    setTimeout(() => {
+        shrinkOverlay();
+    }, 1300); 
 }
 
 function openmodal() {
@@ -28,19 +42,21 @@ function openmodal() {
     right.classList.remove('spreadlr')
     top.classList.remove('spreadtb')
     bottom.classList.remove('spreadtb')
+   
     setTimeout(() => {
-       chk();
-        // logo.classList.add('mv');
-    }, 1000);  // Matches animation duration
+     
+    }, 1000);  
 }
 
 function openlogo(){
     const logo = document.getElementById('logo');
+    const celeb = document.getElementById('celeb');
     logo.classList.add('icon');
+    
     setTimeout(() => {
        
-        
-       
+        chk();
+       celeb.style.display = "block"
         
     }, 2000); 
 }
@@ -51,21 +67,19 @@ function spread() {
     const left = document.getElementById('left');
     const right = document.getElementById('right');
    
-    con.classList.add('con');  // Add the class to trigger the shrinking animation
-    left.classList.add('spreadlr');  // Add the class to trigger the shrinking animation
-    right.classList.add('spreadlr');  // Add the class to trigger the shrinking animation
+    con.classList.add('con');  
+    left.classList.add('spreadlr');  
+    right.classList.add('spreadlr');  
     const top = document.getElementById('top');
     const bottom = document.getElementById('bottom');
-    top.classList.add('spreadtb');  // Add the class to trigger the shrinking animation
+    top.classList.add('spreadtb'); 
     bottom.classList.add('spreadtb');
-       // Add the class to trigger the shrinking animation
-   
-    // Optionally hide the overlay after the animation completes
+     
     setTimeout(() => {
         openmodal()
         openlogo()
       
-    }, 2000);  // Matches animation duration
+    }, 2000);
 }
 
 
@@ -90,4 +104,75 @@ function chk (){
     inp2.classList.add('up');
     ck.classList.add('up');
     ck2.classList.add('up');
+}
+
+
+function mutatecheckbox(){
+    const box = document.getElementById('checkbox');
+    if(box.innerHTML === ''){
+
+        box.innerHTML = '&#10004;'
+    }
+    else{
+        box.innerHTML = ''
+    }
+}
+
+function changeborder(x){
+    const inp1 = document.getElementById('inp1');
+    const inp2 = document.getElementById('inp2');
+    if(x===1){
+        inp1.style.border = '1px solid #50dfc7'
+        inp2.style.border = '1px solid #868686'
+    }
+    else{
+         inp2.style.border = '1px solid #50dfc7'
+        inp1.style.border = '1px solid #868686'
+    }
+
+}
+
+
+function validateinput(){
+    const inp1 = document.getElementById('i1').value;
+    
+    const inp2 = document.getElementById('i2').value;
+    const btn = document.getElementById('btn');
+
+    if(inp1 && inp2){
+        console.log(inp1, inp2)
+        btn.style.backgroundColor="#007663";
+        btn.disabled = false;
+    }
+    else{
+
+       
+        btn.style.backgroundColor="#919292";
+        btn.disabled = true;
+    }
+}
+
+
+function label1(){
+    const inp1 = document.getElementById('i1');
+    
+    
+    if(inp1.value){
+        inp1.classList.add('has-content');
+    }
+    else{
+        inp1.classList.remove('has-content');
+    }
+}
+
+function label2(){
+
+    const inp2 = document.getElementById('i2');
+    
+    if(inp2.value){
+        inp2.classList.add('has-content');
+    }
+    else{
+        inp2.classList.remove('has-content');
+    }
 }
